852bfa3d8a6539f0fb6b71f35b3ba8f7bc3918e2
Bruno Matias Casas <brunomatias0422@gmail.com>
2022-05-01 15:44:50 -0300

    Create new users/books m:n relation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author, optional: false
 
+  has_and_belongs_to_many :users
+
   validates_presence_of :title, :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          :registerable,
          jwt_revocation_strategy: JwtDenylist
+
+  has_and_belongs_to_many :books
+
+  has_many :favorite_books, through: :users_books, source: :book
 end
